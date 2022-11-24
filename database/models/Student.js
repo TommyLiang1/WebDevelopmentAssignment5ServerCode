6a6c7d25befd07975382e015c492d9e95d4cca38
kevinville <kevinlhardyal@gmail.com>
2022-11-23 20:58:17 -0500

    Added email, imageurl, and gpa to students

--- database/models/Student.js
+++ database/models/Student.js
@@ -15,7 +15,21 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
-  }
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+  },
+
+  gpa: {
+    type: Sequelize.FLOAT,
+  },
+
 });
 
 // Export the student model
